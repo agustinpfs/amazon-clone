89c64e2ee06d0475720d46bfda369f145a09c3b6
Agustin <agu@cursosit.com>
2020-12-10 16:44:46 -0300

    add Payment component and install cloud functions through firebase(functions folder)

--- src/App.js
+++ src/App.js
@@ -8,6 +8,13 @@ import React, { useEffect } from 'react';
 import { auth } from "./firebase";
 import { useStateValue } from './StateProvider';
 import Payment from './Payment';
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+const promise = loadStripe(
+  "pk_test_k5pJqJuhTRP8ga91igFpiAar"
+);
+
 
 
 
@@ -60,7 +67,9 @@ function App() {
          
           <Route path='/payment'>
             <Header />
-            <Payment />
+            <Elements stripe={promise}>
+              <Payment />
+            </Elements>
           </Route>
           
           <Route path='/'>
